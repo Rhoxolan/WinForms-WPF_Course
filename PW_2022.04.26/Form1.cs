f4c0a4e0381f68656631835952804c41c1c654ac
Rhoxolan <pba17mail@gmail.com>
2022-04-27 10:37:06 +0300

    Add to PW

--- PW_2022.04.26/Form1.cs
+++ PW_2022.04.26/Form1.cs
@@ -6,5 +6,17 @@ namespace PW_2022._04._26
         {
             InitializeComponent();
         }
+
+        private void Task1_Click(object sender, EventArgs e)
+        {
+            const int string_count = 3;
+            string str1 = "Бацемакин Павел";
+            string str2 = "C# developer";
+            string str3 = "Компьютерная академия ШАГ, г. Кривой Рог";
+            int stringcount = (str1.Length + str2.Length + str3.Length) / string_count;
+            MessageBox.Show(str1, "Резюме");
+            MessageBox.Show(str2, "Резюме");
+            MessageBox.Show($"{str3}{Environment.NewLine}(В резюме {stringcount} символов.)", "Резюме");
+        }
     }
 }
--- PW_2022.04.26/Program.cs
+++ PW_2022.04.26/Program.cs
@@ -8,8 +8,6 @@ namespace PW_2022._04._26
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
             Application.Run(new Form1());
         }
