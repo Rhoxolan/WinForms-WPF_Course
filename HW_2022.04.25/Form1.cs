e2046eb54c19cf14484e92c189b32d708ee4d7a5
Rhoxolan <pba17mail@gmail.com>
2022-04-28 12:23:51 +0300

    Add to HW

--- HW_2022.04.25/Form1.cs
+++ HW_2022.04.25/Form1.cs
@@ -9,7 +9,47 @@ namespace HW_2022._04._25
 
         private void Game_Click(object sender, EventArgs e)
         {
-
+            Random random = new Random();
+            while(true)
+            {
+                DialogResult dr;
+                dr = MessageBox.Show("Загадайте число от 1 до 2000!", "Игра", MessageBoxButtons.OKCancel);
+                if (dr == DialogResult.OK)
+                {
+                    DialogResult dr2;
+                    while (true)
+                    {
+                        DialogResult dr3;
+                        dr2 = MessageBox.Show($"Вы загадали число {random.Next(0,2001)}?", "Игра",
+                            MessageBoxButtons.YesNoCancel);
+                        if(dr2 == DialogResult.Yes)
+                        {
+                            dr2 = MessageBox.Show($"Продолжить игру?", "Игра",
+                            MessageBoxButtons.YesNoCancel);
+                            if (dr2 == DialogResult.Yes)
+                            {
+                                break;
+                            }
+                            if (dr2 == DialogResult.No)
+                            {
+                                return;
+                            }
+                            if (dr2 == DialogResult.Cancel)
+                            {
+                                return;
+                            }
+                        }
+                        if (dr2 == DialogResult.Cancel)
+                        {
+                            return;
+                        }
+                    }
+                }
+                if (dr == DialogResult.Cancel)
+                {
+                    return;
+                }
+            }
         }
     }
 }
