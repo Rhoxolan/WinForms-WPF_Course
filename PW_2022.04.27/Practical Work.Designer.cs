e061b6339bfdd37fbb23d67ea4163141e68a229c
Rhoxolan <pba17mail@gmail.com>
2022-04-29 20:27:10 +0300

    Add to PW

--- PW_2022.04.27/Practical Work.Designer.cs
+++ PW_2022.04.27/Practical Work.Designer.cs
@@ -34,6 +34,18 @@
             this.label2 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.label6 = new System.Windows.Forms.Label();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.SaveClick = new System.Windows.Forms.Button();
+            this.LoadNReadClick = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -84,11 +96,136 @@
             this.label3.TabIndex = 4;
             this.label3.Text = "Отчество";
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 159);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(52, 19);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Муж";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 141);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 15);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Пол";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(70, 159);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(50, 19);
+            this.checkBox2.TabIndex = 8;
+            this.checkBox2.Text = "Жен";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 181);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(120, 15);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Год и дата рождения";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(12, 199);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(108, 23);
+            this.dateTimePicker1.TabIndex = 10;
+            this.dateTimePicker1.Value = new System.DateTime(1990, 1, 1, 10, 10, 0, 0);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 225);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(103, 15);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Семейный статус";
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(12, 243);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(124, 19);
+            this.checkBox3.TabIndex = 12;
+            this.checkBox3.Text = "Замужем (Женат)";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(142, 243);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(101, 19);
+            this.checkBox4.TabIndex = 13;
+            this.checkBox4.Text = "Свободен(на)";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 265);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(175, 15);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Дополнительная информация";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(12, 283);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(231, 90);
+            this.textBox4.TabIndex = 15;
+            // 
+            // SaveClick
+            // 
+            this.SaveClick.Location = new System.Drawing.Point(12, 379);
+            this.SaveClick.Name = "SaveClick";
+            this.SaveClick.Size = new System.Drawing.Size(83, 23);
+            this.SaveClick.TabIndex = 16;
+            this.SaveClick.Text = "Save";
+            this.SaveClick.UseVisualStyleBackColor = true;
+            this.SaveClick.Click += new System.EventHandler(this.SaveClick_Click);
+            // 
+            // LoadNReadClick
+            // 
+            this.LoadNReadClick.Location = new System.Drawing.Point(12, 408);
+            this.LoadNReadClick.Name = "LoadNReadClick";
+            this.LoadNReadClick.Size = new System.Drawing.Size(83, 23);
+            this.LoadNReadClick.TabIndex = 17;
+            this.LoadNReadClick.Text = "Load n Read";
+            this.LoadNReadClick.UseVisualStyleBackColor = true;
+            this.LoadNReadClick.Click += new System.EventHandler(this.LoadNReadClick_Click);
+            // 
             // Practical_Work
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(504, 441);
+            this.Controls.Add(this.LoadNReadClick);
+            this.Controls.Add(this.SaveClick);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox2);
@@ -110,5 +247,17 @@
         private Label label2;
         private TextBox textBox3;
         private Label label3;
+        private CheckBox checkBox1;
+        private Label label4;
+        private CheckBox checkBox2;
+        private Label label5;
+        private DateTimePicker dateTimePicker1;
+        private Label label6;
+        private CheckBox checkBox3;
+        private CheckBox checkBox4;
+        private Label label7;
+        private TextBox textBox4;
+        private Button SaveClick;
+        private Button LoadNReadClick;
     }
 }
--- PW_2022.04.27/Practical Work.cs
+++ PW_2022.04.27/Practical Work.cs
@@ -1,10 +1,61 @@
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text.Json;
+
 namespace PW_2022._04._27
 {
+    record PersonInfo(string? Name, string? Surname, string? Patronymic, byte? Gender, DateTime? Date_Time,
+        byte? MaritalStatus, string? AddInfo);
+
     public partial class Practical_Work : Form
     {
+        private PersonInfo? personInfo;
+
         public Practical_Work()
         {
+            personInfo = null;
             InitializeComponent();
+        }
+
+        private void SaveClick_Click(object sender, EventArgs e)
+        {
+            byte? gender = null;
+            if (checkBox1.Checked)
+            {
+                gender = 1;
+            }
+            if (checkBox2.Checked)
+            {
+                gender = 2;
+            }
+            byte? marital_status = null;
+            if (checkBox3.Checked)
+            {
+                marital_status = 1;
+            }
+            if (checkBox4.Checked)
+            {
+                marital_status = 2;
+            }
+            DateTime? date = dateTimePicker1.Value;
+            personInfo = new(textBox1.Text, textBox2.Text, textBox3.Text, gender, date, marital_status,
+                textBox4.Text);
+
+            string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            using (FileStream fs = new(Path.Combine(docPath, "personInfo.bin"), FileMode.OpenOrCreate))
+            {
+                JsonSerializer.Serialize<PersonInfo?>(fs, personInfo);
+            }
+            MessageBox.Show("Данные успешно внесены!");
+        }
+
+        private void LoadNReadClick_Click(object sender, EventArgs e)
+        {
+            string docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            using (FileStream fs = new(Path.Combine(docPath, "personInfo.bin"), FileMode.OpenOrCreate))
+            {
+                personInfo = JsonSerializer.Deserialize<PersonInfo>(fs);
+            }
         }
     }
 }
