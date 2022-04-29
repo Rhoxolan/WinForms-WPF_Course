3d9626b23943b2b0646d7d1bb28881dd0a981eac
Rhoxolan <pba17mail@gmail.com>
2022-04-29 18:48:49 +0300

    Add to PW

---
+++ PW_2022.04.27/Practical Work.cs
@@ -0,0 +1,10 @@
+namespace PW_2022._04._27
+{
+    public partial class Practical_Work : Form
+    {
+        public Practical_Work()
+        {
+            InitializeComponent();
+        }
+    }
+}
