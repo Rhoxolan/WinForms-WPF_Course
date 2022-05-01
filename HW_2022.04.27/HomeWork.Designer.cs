78f298a6a091ba7cbf8da38e07523049c08b0dd2
Rhoxolan <pba17mail@gmail.com>
2022-05-01 23:00:19 +0300

    Add HW

---
+++ HW_2022.04.27/HomeWork.Designer.cs
@@ -0,0 +1,46 @@
+﻿namespace HW_2022._04._27
+{
+    partial class HomeWork
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // HomeWork
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Name = "HomeWork";
+            this.Text = "Home Work 27.04.2022";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
