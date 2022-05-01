aa755cfa7d14a2fd5f89818027b32fe8fa47b58d
Rhoxolan <pba17mail@gmail.com>
2022-05-01 23:41:08 +0300

    Add to HW

--- HW_2022.04.27/HomeWork.Designer.cs
+++ HW_2022.04.27/HomeWork.Designer.cs
@@ -28,19 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.YesButton = new System.Windows.Forms.Button();
+            this.NoButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe Print", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.IndianRed;
+            this.label1.Location = new System.Drawing.Point(196, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(361, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Вы довольны своей зарплатой?";
+            // 
+            // YesButton
+            // 
+            this.YesButton.BackColor = System.Drawing.Color.PapayaWhip;
+            this.YesButton.Location = new System.Drawing.Point(88, 105);
+            this.YesButton.Name = "YesButton";
+            this.YesButton.Size = new System.Drawing.Size(141, 53);
+            this.YesButton.TabIndex = 1;
+            this.YesButton.Text = "Да!";
+            this.YesButton.UseVisualStyleBackColor = false;
+            this.YesButton.Click += new System.EventHandler(this.YesButton_Click);
+            // 
+            // NoButton
+            // 
+            this.NoButton.BackColor = System.Drawing.Color.PapayaWhip;
+            this.NoButton.Location = new System.Drawing.Point(493, 105);
+            this.NoButton.Name = "NoButton";
+            this.NoButton.Size = new System.Drawing.Size(141, 53);
+            this.NoButton.TabIndex = 2;
+            this.NoButton.Text = "Нет";
+            this.NoButton.UseVisualStyleBackColor = false;
+            this.NoButton.MouseEnter += new System.EventHandler(this.NoButton_MouseEnter);
             // 
             // HomeWork
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.GhostWhite;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.NoButton);
+            this.Controls.Add(this.YesButton);
+            this.Controls.Add(this.label1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
             this.Name = "HomeWork";
             this.Text = "Home Work 27.04.2022";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Label label1;
+        private Button YesButton;
+        private Button NoButton;
     }
 }
--- HW_2022.04.27/HomeWork.cs
+++ HW_2022.04.27/HomeWork.cs
@@ -2,9 +2,23 @@ namespace HW_2022._04._27
 {
     public partial class HomeWork : Form
     {
+        private Random random;
+
         public HomeWork()
         {
+            random = new();
             InitializeComponent();
+        }
+
+        private void YesButton_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Мы и не сомневались, что вы так думаете!");
+        }
+
+        private void NoButton_MouseEnter(object sender, EventArgs e)
+        {
+            NoButton.Top = random.Next(ClientSize.Height - NoButton.Height);
+            NoButton.Left = random.Next(ClientSize.Width - NoButton.Width);
         }
     }
 }
