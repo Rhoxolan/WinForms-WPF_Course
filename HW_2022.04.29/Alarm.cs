6f1380f8f2ea0a98715579c11bb96912dbb2e4e2
Rhoxolan <pba17mail@gmail.com>
2022-05-03 23:40:30 +0300

    Add to HW

--- HW_2022.04.29/Alarm.cs
+++ HW_2022.04.29/Alarm.cs
@@ -3,7 +3,6 @@ namespace HW_2022._04._29
     public partial class Alarm : Form
     {
         private static System.Windows.Forms.Timer _timer;
-        private DateTime dateTime;
 
         public Alarm()
         {
@@ -12,11 +11,9 @@ namespace HW_2022._04._29
 
         private void ShowTime(object obj, EventArgs e)
         {
-            DateTime dt1 = Convert.ToDateTime(MaskedTextBox.Text);
-            DateTime dt2 = DateTime.Now;
-            if (dt1.Hour == dt2.Hour && dt1.Minute == dt2.Minute)
+            if (Convert.ToDateTime(MaskedTextBox.Text).Hour == DateTime.Now.Hour &&
+                Convert.ToDateTime(MaskedTextBox.Text).Minute == DateTime.Now.Minute)
             {
-                MessageBox.Show("Да");
                 _timer.Stop();
             }
         }
@@ -24,13 +21,12 @@ namespace HW_2022._04._29
         private void Alarm_Load(object sender, EventArgs e)
         {
             _timer = new();
-            dateTime = new();
         }
 
         private void StartButton_Click(object sender, EventArgs e)
         {
             _timer.Tick += new EventHandler(ShowTime);
-            _timer.Interval = 1;
+            _timer.Interval = 1000;
             _timer.Start();
         }
     }
