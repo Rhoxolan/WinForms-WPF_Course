ae4622ac3224cb367d418f5ad6560c0b6dd5d433
Rhoxolan <pba17mail@gmail.com>
2022-05-05 19:07:24 +0300

    Add to PW

--- PW_2022.05.02/Form1.Designer.cs
+++ PW_2022.05.02/Form1.Designer.cs
@@ -29,9 +29,20 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.Form = new System.Windows.Forms.ListBox();
+            this.ListBoxForm = new System.Windows.Forms.ListBox();
             this.AddButton = new System.Windows.Forms.Button();
             this.DelButton = new System.Windows.Forms.Button();
+            this.NameTextBox = new System.Windows.Forms.TextBox();
+            this.NameLabel = new System.Windows.Forms.Label();
+            this.SurnameLabel = new System.Windows.Forms.Label();
+            this.SurnameTextBox = new System.Windows.Forms.TextBox();
+            this.EmailLabel = new System.Windows.Forms.Label();
+            this.EmailTextBox = new System.Windows.Forms.TextBox();
+            this.NumberLabel = new System.Windows.Forms.Label();
+            this.PhoneNumberTextBox = new System.Windows.Forms.TextBox();
+            this.EditButton = new System.Windows.Forms.Button();
+            this.SaveButton = new System.Windows.Forms.Button();
+            this.LoadButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -44,44 +55,152 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Анкета";
             // 
-            // Form
+            // ListBoxForm
             // 
-            this.Form.FormattingEnabled = true;
-            this.Form.ItemHeight = 15;
-            this.Form.Location = new System.Drawing.Point(12, 44);
-            this.Form.Name = "Form";
-            this.Form.Size = new System.Drawing.Size(547, 364);
-            this.Form.TabIndex = 1;
+            this.ListBoxForm.FormattingEnabled = true;
+            this.ListBoxForm.ItemHeight = 15;
+            this.ListBoxForm.Location = new System.Drawing.Point(12, 44);
+            this.ListBoxForm.Name = "ListBoxForm";
+            this.ListBoxForm.Size = new System.Drawing.Size(369, 364);
+            this.ListBoxForm.TabIndex = 1;
             // 
             // AddButton
             // 
-            this.AddButton.Location = new System.Drawing.Point(565, 44);
+            this.AddButton.Location = new System.Drawing.Point(387, 223);
             this.AddButton.Name = "AddButton";
-            this.AddButton.Size = new System.Drawing.Size(81, 28);
+            this.AddButton.Size = new System.Drawing.Size(115, 28);
             this.AddButton.TabIndex = 2;
             this.AddButton.Text = "Добавить";
             this.AddButton.UseVisualStyleBackColor = true;
+            this.AddButton.Click += new System.EventHandler(this.AddButton_Click);
             // 
             // DelButton
             // 
-            this.DelButton.Location = new System.Drawing.Point(565, 78);
+            this.DelButton.Location = new System.Drawing.Point(387, 257);
             this.DelButton.Name = "DelButton";
-            this.DelButton.Size = new System.Drawing.Size(81, 28);
+            this.DelButton.Size = new System.Drawing.Size(115, 28);
             this.DelButton.TabIndex = 3;
             this.DelButton.Text = "Удалить";
             this.DelButton.UseVisualStyleBackColor = true;
+            this.DelButton.Click += new System.EventHandler(this.DelButton_Click);
+            // 
+            // NameTextBox
+            // 
+            this.NameTextBox.Location = new System.Drawing.Point(387, 62);
+            this.NameTextBox.Name = "NameTextBox";
+            this.NameTextBox.Size = new System.Drawing.Size(200, 23);
+            this.NameTextBox.TabIndex = 4;
+            // 
+            // NameLabel
+            // 
+            this.NameLabel.AutoSize = true;
+            this.NameLabel.Location = new System.Drawing.Point(387, 44);
+            this.NameLabel.Name = "NameLabel";
+            this.NameLabel.Size = new System.Drawing.Size(31, 15);
+            this.NameLabel.TabIndex = 5;
+            this.NameLabel.Text = "Имя";
+            // 
+            // SurnameLabel
+            // 
+            this.SurnameLabel.AutoSize = true;
+            this.SurnameLabel.Location = new System.Drawing.Point(387, 88);
+            this.SurnameLabel.Name = "SurnameLabel";
+            this.SurnameLabel.Size = new System.Drawing.Size(58, 15);
+            this.SurnameLabel.TabIndex = 7;
+            this.SurnameLabel.Text = "Фамилия";
+            // 
+            // SurnameTextBox
+            // 
+            this.SurnameTextBox.Location = new System.Drawing.Point(387, 106);
+            this.SurnameTextBox.Name = "SurnameTextBox";
+            this.SurnameTextBox.Size = new System.Drawing.Size(200, 23);
+            this.SurnameTextBox.TabIndex = 6;
+            // 
+            // EmailLabel
+            // 
+            this.EmailLabel.AutoSize = true;
+            this.EmailLabel.Location = new System.Drawing.Point(387, 132);
+            this.EmailLabel.Name = "EmailLabel";
+            this.EmailLabel.Size = new System.Drawing.Size(41, 15);
+            this.EmailLabel.TabIndex = 9;
+            this.EmailLabel.Text = "E-Mail";
+            // 
+            // EmailTextBox
+            // 
+            this.EmailTextBox.Location = new System.Drawing.Point(387, 150);
+            this.EmailTextBox.Name = "EmailTextBox";
+            this.EmailTextBox.Size = new System.Drawing.Size(200, 23);
+            this.EmailTextBox.TabIndex = 8;
+            // 
+            // NumberLabel
+            // 
+            this.NumberLabel.AutoSize = true;
+            this.NumberLabel.Location = new System.Drawing.Point(387, 176);
+            this.NumberLabel.Name = "NumberLabel";
+            this.NumberLabel.Size = new System.Drawing.Size(101, 15);
+            this.NumberLabel.TabIndex = 13;
+            this.NumberLabel.Text = "Номер телефона";
+            // 
+            // PhoneNumberTextBox
+            // 
+            this.PhoneNumberTextBox.Location = new System.Drawing.Point(387, 194);
+            this.PhoneNumberTextBox.Name = "PhoneNumberTextBox";
+            this.PhoneNumberTextBox.Size = new System.Drawing.Size(200, 23);
+            this.PhoneNumberTextBox.TabIndex = 12;
+            // 
+            // EditButton
+            // 
+            this.EditButton.Location = new System.Drawing.Point(387, 291);
+            this.EditButton.Name = "EditButton";
+            this.EditButton.Size = new System.Drawing.Size(115, 28);
+            this.EditButton.TabIndex = 14;
+            this.EditButton.Text = "Редактировать";
+            this.EditButton.UseVisualStyleBackColor = true;
+            this.EditButton.Click += new System.EventHandler(this.EditButton_Click);
+            // 
+            // SaveButton
+            // 
+            this.SaveButton.Location = new System.Drawing.Point(387, 346);
+            this.SaveButton.Name = "SaveButton";
+            this.SaveButton.Size = new System.Drawing.Size(115, 28);
+            this.SaveButton.TabIndex = 15;
+            this.SaveButton.Text = "Сохранить в XML";
+            this.SaveButton.UseVisualStyleBackColor = true;
+            this.SaveButton.Click += new System.EventHandler(this.SaveButton_Click);
+            // 
+            // LoadButton
+            // 
+            this.LoadButton.Location = new System.Drawing.Point(387, 380);
+            this.LoadButton.Name = "LoadButton";
+            this.LoadButton.Size = new System.Drawing.Size(115, 28);
+            this.LoadButton.TabIndex = 16;
+            this.LoadButton.Text = "Загрзить";
+            this.LoadButton.UseVisualStyleBackColor = true;
+            this.LoadButton.Click += new System.EventHandler(this.LoadButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(665, 450);
+            this.ClientSize = new System.Drawing.Size(600, 450);
+            this.Controls.Add(this.LoadButton);
+            this.Controls.Add(this.SaveButton);
+            this.Controls.Add(this.EditButton);
+            this.Controls.Add(this.NumberLabel);
+            this.Controls.Add(this.PhoneNumberTextBox);
+            this.Controls.Add(this.EmailLabel);
+            this.Controls.Add(this.EmailTextBox);
+            this.Controls.Add(this.SurnameLabel);
+            this.Controls.Add(this.SurnameTextBox);
+            this.Controls.Add(this.NameLabel);
+            this.Controls.Add(this.NameTextBox);
             this.Controls.Add(this.DelButton);
             this.Controls.Add(this.AddButton);
-            this.Controls.Add(this.Form);
+            this.Controls.Add(this.ListBoxForm);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -90,8 +209,19 @@
         #endregion
 
         private Label label1;
-        private ListBox Form;
+        private ListBox ListBoxForm;
         private Button AddButton;
         private Button DelButton;
+        private TextBox NameTextBox;
+        private Label NameLabel;
+        private Label SurnameLabel;
+        private TextBox SurnameTextBox;
+        private Label EmailLabel;
+        private TextBox EmailTextBox;
+        private Label NumberLabel;
+        private TextBox PhoneNumberTextBox;
+        private Button EditButton;
+        private Button SaveButton;
+        private Button LoadButton;
     }
 }
--- PW_2022.05.02/Form1.cs
+++ PW_2022.05.02/Form1.cs
@@ -1,13 +1,69 @@
+using System.Xml.Serialization;
+using System.Text.Json;
+using System.Collections;
+
 namespace PW_2022._05._02
 {
     public partial class Form1 : Form
     {
+        private string docPath;
+
         public Form1()
         {
             InitializeComponent();
         }
-    }
 
+        private void AddButton_Click(object sender, EventArgs e)
+        {
+            if (NameTextBox.Text.Length > 0 && SurnameTextBox.Text.Length > 0 &&
+                EmailTextBox.Text.Length > 0 && PhoneNumberTextBox.Text.Length > 0)
+            {
+                ListBoxForm.Items.Add(new Person(NameTextBox.Text, SurnameTextBox.Text, EmailTextBox.Text,
+                PhoneNumberTextBox.Text));
+            }
+        }
+
+        private void DelButton_Click(object sender, EventArgs e)
+        {
+            if (ListBoxForm.SelectedIndex != -1)
+            {
+                ListBoxForm.Items.RemoveAt(ListBoxForm.SelectedIndex);
+            }
+        }
+
+        private void EditButton_Click(object sender, EventArgs e)
+        {
+            if (ListBoxForm.SelectedIndex != -1)
+            {
+                ListBoxForm.Items[ListBoxForm.SelectedIndex] = new Person(NameTextBox.Text, SurnameTextBox.Text, EmailTextBox.Text,
+                PhoneNumberTextBox.Text);
+            }
+        }
+
+        private void SaveButton_Click(object sender, EventArgs e)
+        {
+            ArrayList al = new(ListBoxForm.Items);
+            //Попробовать это сериализовать
+
+            //Отлично сериализуется
+            //using (FileStream fs = new(Path.Combine(docPath, "personInfo.json"), FileMode.Create))
+            //{
+            //    JsonSerializer.Serialize(fs, ListBoxForm.Items);
+            //}
+
+            //Попробовать десиаризовать в аррей лист, затем привязать к листбоксу
+        }
+
+        private void LoadButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            docPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+        }
+    }
     class Person
     {
         public string Name { get; set; }
