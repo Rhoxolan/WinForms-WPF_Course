8eb2116a65dbd1d5507c6df6c48ccfbf3e2c4ca1
Rhoxolan <pba17mail@gmail.com>
2022-05-20 20:24:59 +0300

    Add to HW

--- PW_2022.05.11/Form1.Designer.cs
+++ PW_2022.05.11/Form1.Designer.cs
@@ -28,18 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.labelTask1 = new System.Windows.Forms.Label();
             this.treeViewExplorer = new System.Windows.Forms.TreeView();
+            this.listViewExplorer = new System.Windows.Forms.ListView();
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.largeIconToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.detailsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.smallIconToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.labelTask2 = new System.Windows.Forms.Label();
+            this.imageList = new System.Windows.Forms.ImageList(this.components);
+            this.contextMenuStrip.SuspendLayout();
             this.SuspendLayout();
             // 
-            // label1
+            // labelTask1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(61, 15);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Задание 1";
+            this.labelTask1.AutoSize = true;
+            this.labelTask1.Location = new System.Drawing.Point(12, 9);
+            this.labelTask1.Name = "labelTask1";
+            this.labelTask1.Size = new System.Drawing.Size(61, 15);
+            this.labelTask1.TabIndex = 0;
+            this.labelTask1.Text = "Задание 1";
             // 
             // treeViewExplorer
             // 
@@ -48,16 +60,91 @@
             this.treeViewExplorer.Size = new System.Drawing.Size(299, 411);
             this.treeViewExplorer.TabIndex = 1;
             // 
+            // listViewExplorer
+            // 
+            this.listViewExplorer.ContextMenuStrip = this.contextMenuStrip;
+            this.listViewExplorer.Location = new System.Drawing.Point(317, 27);
+            this.listViewExplorer.Name = "listViewExplorer";
+            this.listViewExplorer.Size = new System.Drawing.Size(297, 411);
+            this.listViewExplorer.TabIndex = 2;
+            this.listViewExplorer.UseCompatibleStateImageBehavior = false;
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.largeIconToolStripMenuItem,
+            this.detailsToolStripMenuItem,
+            this.smallIconToolStripMenuItem,
+            this.listToolStripMenuItem,
+            this.tileToolStripMenuItem});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(127, 114);
+            this.contextMenuStrip.Text = "Вид";
+            // 
+            // largeIconToolStripMenuItem
+            // 
+            this.largeIconToolStripMenuItem.Name = "largeIconToolStripMenuItem";
+            this.largeIconToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
+            this.largeIconToolStripMenuItem.Text = "LargeIcon";
+            this.largeIconToolStripMenuItem.Click += new System.EventHandler(this.largeIconToolStripMenuItem_Click);
+            // 
+            // detailsToolStripMenuItem
+            // 
+            this.detailsToolStripMenuItem.Name = "detailsToolStripMenuItem";
+            this.detailsToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
+            this.detailsToolStripMenuItem.Text = "Details";
+            this.detailsToolStripMenuItem.Click += new System.EventHandler(this.detailsToolStripMenuItem_Click);
+            // 
+            // smallIconToolStripMenuItem
+            // 
+            this.smallIconToolStripMenuItem.Name = "smallIconToolStripMenuItem";
+            this.smallIconToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
+            this.smallIconToolStripMenuItem.Text = "SmallIcon";
+            this.smallIconToolStripMenuItem.Click += new System.EventHandler(this.smallIconToolStripMenuItem_Click);
+            // 
+            // listToolStripMenuItem
+            // 
+            this.listToolStripMenuItem.Name = "listToolStripMenuItem";
+            this.listToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
+            this.listToolStripMenuItem.Text = "List";
+            this.listToolStripMenuItem.Click += new System.EventHandler(this.listToolStripMenuItem_Click);
+            // 
+            // tileToolStripMenuItem
+            // 
+            this.tileToolStripMenuItem.Name = "tileToolStripMenuItem";
+            this.tileToolStripMenuItem.Size = new System.Drawing.Size(126, 22);
+            this.tileToolStripMenuItem.Text = "Tile";
+            this.tileToolStripMenuItem.Click += new System.EventHandler(this.tileToolStripMenuItem_Click);
+            // 
+            // labelTask2
+            // 
+            this.labelTask2.AutoSize = true;
+            this.labelTask2.Location = new System.Drawing.Point(317, 9);
+            this.labelTask2.Name = "labelTask2";
+            this.labelTask2.Size = new System.Drawing.Size(61, 15);
+            this.labelTask2.TabIndex = 3;
+            this.labelTask2.Text = "Задание 2";
+            // 
+            // imageList
+            // 
+            this.imageList.ColorDepth = System.Windows.Forms.ColorDepth.Depth24Bit;
+            this.imageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList.ImageStream")));
+            this.imageList.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList.Images.SetKeyName(0, "223085882.png");
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(627, 450);
+            this.Controls.Add(this.labelTask2);
+            this.Controls.Add(this.listViewExplorer);
             this.Controls.Add(this.treeViewExplorer);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.labelTask1);
             this.Name = "Form1";
             this.Text = "Practical Work";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.contextMenuStrip.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -65,7 +152,16 @@
 
         #endregion
 
-        private Label label1;
+        private Label labelTask1;
         private TreeView treeViewExplorer;
+        private ListView listViewExplorer;
+        private Label labelTask2;
+        private ContextMenuStrip contextMenuStrip;
+        private ToolStripMenuItem largeIconToolStripMenuItem;
+        private ToolStripMenuItem detailsToolStripMenuItem;
+        private ToolStripMenuItem smallIconToolStripMenuItem;
+        private ToolStripMenuItem listToolStripMenuItem;
+        private ToolStripMenuItem tileToolStripMenuItem;
+        private ImageList imageList;
     }
 }
--- PW_2022.05.11/Form1.cs
+++ PW_2022.05.11/Form1.cs
@@ -9,7 +9,8 @@ namespace PW_2022._05._11
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            foreach(DriveInfo drive in DriveInfo.GetDrives())
+            //TreeView
+            foreach (DriveInfo drive in DriveInfo.GetDrives())
             {
                 TreeNode tn = treeViewExplorer.Nodes.Add(drive.Name);
                 if (drive.IsReady)
@@ -20,6 +21,50 @@ namespace PW_2022._05._11
                     }
                 }
             }
+
+            //ListView
+            listViewExplorer.SmallImageList = imageList;
+            listViewExplorer.LargeImageList = imageList;
+            listViewExplorer.StateImageList = imageList;
+            listViewExplorer.GroupImageList = imageList;
+            listViewExplorer.Columns.Add("Title");
+            listViewExplorer.Columns.Add("Type");
+            listViewExplorer.Columns.Add("Size");
+            foreach (DriveInfo drive in DriveInfo.GetDrives())
+            {
+                ListViewItem driveItem = listViewExplorer.Items.Add(drive.Name);
+                driveItem.ImageIndex = 0;
+                if (drive.IsReady)
+                {
+                    driveItem.SubItems.Add(drive.DriveType.ToString());
+                    driveItem.SubItems.Add(drive.TotalSize.ToString());
+                }
+            }
+        }
+
+        private void largeIconToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listViewExplorer.View = View.LargeIcon;
+        }
+
+        private void detailsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listViewExplorer.View = View.Details;
+        }
+
+        private void smallIconToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listViewExplorer.View = View.SmallIcon;
+        }
+
+        private void listToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listViewExplorer.View = View.List;
+        }
+
+        private void tileToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            listViewExplorer.View = View.Tile;
         }
     }
 }
