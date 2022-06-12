ecbf13e1c1def6fc72dfd2ae3104aa061bba0c0b
Rhoxolan <pba17mail@gmail.com>
2022-06-12 22:14:59 +0300

    Add HW

---
+++ HW_2022.05.20_WPF/MainWindow.xaml.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HW_2022._05._20_WPF
+{
+    public partial class MainWindow : Window
+    {
+        private bool isEqualsOut;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            isEqualsOut = false;
+        }
+
+        private void OneButton_Click(object sender, EventArgs e)
+        {
+            if (isEqualsOut)
+            {
+                TextBox.Text += "1";
+            }
+            else
+            {
+                TextBox.Text = "1";
+                isEqualsOut = true;
+            }
+        }
+
+        private void TwoButton_Click(object sender, EventArgs e)
+        {
+            if (isEqualsOut)
+            {
+                TextBox.Text += "2";
+            }
+            else
+            {
+                TextBox.Text = "2";
+                isEqualsOut = true;
+            }
+        }
+
+        private void ThreeButton_Click(object sender, EventArgs e)
+        {
+            if (isEqualsOut)
+            {
+                TextBox.Text += "3";
+            }
+            else
+            {
+                TextBox.Text = "3";
+                isEqualsOut = true;
+            }
+        }
+
+        private void FourButton_Click(object sender, EventArgs e)
+        {
+            if (isEqualsOut)
+            {
+                TextBox.Text += "4";
+            }
+            else
+            {
+                TextBox.Text = "4";
+                isEqualsOut = true;
+            }
+        }
+
+        private void FiveButton_Click(object sender, EventArgs e)
+        {
+            if (isEqualsOut)
+            {
+                TextBox.Text += "5";
+            }
+            else
+            {
+                TextBox.Text = "5";
+                isEqualsOut = true;
+            }
+        }
+
+        private void SixButton_Click(object sender, EventArgs e)
+        {
+            if (isEqualsOut)
+            {
+                TextBox.Text += "6";
+            }
+            else
+            {
+                TextBox.Text = "6";
+                isEqualsOut = true;
+            }
+        }
+
+        private void SevenButton_Click(object sender, EventArgs e)
+        {
+            if (isEqualsOut)
+            {
+                TextBox.Text += "7";
+            }
+            else
+            {
+                TextBox.Text = "7";
+                isEqualsOut = true;
+            }
+        }
+
+        private void EightButton_Click(object sender, EventArgs e)
+        {
+            if (isEqualsOut)
+            {
+                TextBox.Text += "8";
+            }
+            else
+            {
+                TextBox.Text = "8";
+                isEqualsOut = true;
+            }
+        }
+
+        private void NineButton_Click(object sender, EventArgs e)
+        {
+            if (isEqualsOut)
+            {
+                TextBox.Text += "9";
+            }
+            else
+            {
+                TextBox.Text = "9";
+                isEqualsOut = true;
+            }
+        }
+
+        private void ZeroButton_Click(object sender, EventArgs e)
+        {
+            if (isEqualsOut)
+            {
+                TextBox.Text += "0";
+            }
+            else
+            {
+                TextBox.Text = "0";
+                isEqualsOut = true;
+            }
+        }
+
+        private void PlusButton_Click(object sender, EventArgs e)
+        {
+            if (isEqualsOut)
+            {
+                TextBox.Text += "+";
+            }
+            else
+            {
+                TextBox.Text = "+";
+                isEqualsOut = true;
+            }
+        }
+
+        private void MinusButton_Click(object sender, EventArgs e)
+        {
+            if (isEqualsOut)
+            {
+                TextBox.Text += "-";
+            }
+            else
+            {
+                TextBox.Text = "-";
+                isEqualsOut = true;
+            }
+        }
+
+        private void SubButton_Click(object sender, EventArgs e)
+        {
+            if (isEqualsOut)
+            {
+                TextBox.Text += "*";
+            }
+            else
+            {
+                TextBox.Text = "*";
+                isEqualsOut = true;
+            }
+        }
+
+        private void DivisionButton_Click(object sender, EventArgs e)
+        {
+            if (isEqualsOut)
+            {
+                TextBox.Text += "/";
+            }
+            else
+            {
+                TextBox.Text = "/";
+                isEqualsOut = true;
+            }
+        }
+
+        private void PointButton_Click(object sender, EventArgs e)
+        {
+            if (isEqualsOut)
+            {
+                TextBox.Text += ".";
+            }
+            else
+            {
+                TextBox.Text = ".";
+                isEqualsOut = true;
+            }
+        }
+
+        private void EqualsButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataTable table = new DataTable();
+                table.Columns.Add("expression", typeof(string), TextBox.Text);
+                DataRow row = table.NewRow();
+                table.Rows.Add(row);
+                TextBox.Text = (string)row["expression"];
+                isEqualsOut = false;
+            }
+            catch
+            {
+                TextBox.Text = "Ошибка ввода! Пожалуйста, проверьте корректность ввода";
+                isEqualsOut = false;
+            }
+        }
+
+        private void CButton_Click(object sender, EventArgs e)
+        {
+            TextBox.Text = "";
+        }
+
+        private void DelButton_Click(object sender, EventArgs e)
+        {
+            if (TextBox.Text.Length != 0)
+            {
+                StringBuilder sb = new StringBuilder(TextBox.Text);
+                sb.Remove(sb.Length - 1, 1);
+                TextBox.Text = sb.ToString();
+            }
+        }
+
+        //Меняем последний знак
+        private void ChangeButton_Click(object sender, EventArgs e)
+        {
+            StringBuilder sb = new StringBuilder(TextBox.Text);
+            for (int i = TextBox.Text.Length - 1; i >= 0; i--)
+            {
+                if (sb[i] == '+')
+                {
+                    sb[i] = '-';
+                    TextBox.Text = sb.ToString();
+                    break;
+                }
+                if (sb[i] == '-')
+                {
+                    sb[i] = '+';
+                    TextBox.Text = sb.ToString();
+                    break;
+                }
+            }
+        }
+    }
+}
