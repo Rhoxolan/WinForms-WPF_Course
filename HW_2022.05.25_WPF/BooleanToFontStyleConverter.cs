758b966b755248f2caeeeb96b865fc0557ccbf46
Rhoxolan <pba17mail@gmail.com>
2022-06-16 20:14:34 +0300

    Add to HW

---
+++ HW_2022.05.25_WPF/BooleanToFontStyleConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace HW_2022._05._25_WPF
+{
+    public class BooleanToFontStyleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (parameter != null && (bool)parameter == true)
+            {
+                return FontStyles.Italic;
+            }
+            else
+            {
+                return FontStyles.Normal;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
