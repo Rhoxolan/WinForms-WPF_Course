d91dee967253c9d18688c8ee7461370eff37ba1f
Rhoxolan <pba17mail@gmail.com>
2022-06-22 12:01:13 +0300

    Add to PW

---
+++ PW_2022.05.27_WPF/IntToStringConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace PW_2022._05._27_WPF
+{
+    public class IntToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+                return value.ToString();
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
