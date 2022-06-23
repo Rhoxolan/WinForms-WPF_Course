2c6137e23e6740b5e00def4a3f40f1ee769506bc
Rhoxolan <pba17mail@gmail.com>
2022-06-23 11:29:24 +0300

    Add PW

---
+++ PW_2022.05.27_WPF_V2/ApplicationViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+using System.IO;
+using Microsoft.Win32;
+
+namespace PW_2022._05._27_WPF_V2
+{
+    public class ApplicationViewModel : INotifyPropertyChanged
+    {
+        public BindingList<ShowedImage> Images { get; set; }
+
+        private ShowedImage imageShowed;
+
+        public ApplicationViewModel()
+        {
+            Images = new BindingList<ShowedImage>();
+            imageShowed = null;
+        }
+
+        public ShowedImage ImageShowed
+        {
+            get { return imageShowed; }
+            set
+            {
+                imageShowed = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+    }
+}
---
+++ PW_2022.05.27_WPF_V2/ShowedImage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.IO;
+
+namespace PW_2022._05._27_WPF_V2
+{
+    public class ShowedImage
+    {
+        private string Path { get; set; }
+        private FileInfo Fileinfo { get; set; }
+    }
+}
