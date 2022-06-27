6684404aa4ec69c3378de2ffae7f0886f5085b71
Rhoxolan <pba17mail@gmail.com>
2022-06-27 20:22:13 +0300

    Add PW

---
+++ PW_2022.06.01_WPF/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PW_2022._06._01_WPF
+{
+    public partial class MainWindow : Window
+    {
+        private BindingList<string> styles;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            styles = new BindingList<string>();
+            styles.Add("Style 1");
+            styles.Add("Style 2");
+            styles.Add("Style 3");
+            comboBoxStyles.ItemsSource = styles;
+            comboBoxStyles.SelectedItem = "Style 1";
+        }
+
+        private void buttonClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
